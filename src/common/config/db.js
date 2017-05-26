6af2049e1801292a9eb0f66399fab4840f70aeca
yangsheng <yangsheng@doupai.cc>
2017-05-26 16:07:50 +0800

    增加node_module

--- src/common/config/db.js
+++ src/common/config/db.js
@@ -1,7 +1,7 @@
+"use strict";
 
-      "use strict";
-      exports.__esModule = true;
-      exports.default = {
+exports.__esModule = true;
+exports.default = {
     "type": "mysql",
     "adapter": {
         "mysql": {
