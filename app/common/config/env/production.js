30493e1816a894e7df6c890df48fd4441f541ed5
渴鱼 <738613780@qq.com>
2017-05-31 13:58:08 +0800

    变更prd配置

--- app/common/config/env/production.js
+++ app/common/config/env/production.js
@@ -2,6 +2,15 @@
 
 exports.__esModule = true;
 exports.default = {
-  cluster_on: 1
+    cluster_on: 1,
+    mysql: {
+        "host": "39.108.68.144",
+        "port": "3306",
+        "database": "blog",
+        "user": "root",
+        "password": "a13660858829",
+        "prefix": "fk_",
+        "type": "mysql"
+    }
 };
 //# sourceMappingURL=production.js.map
--- src/common/config/env/production.js
+++ src/common/config/env/production.js
@@ -1,5 +1,14 @@
 'use strict';
 
 export default {
-  cluster_on: 1
+    cluster_on: 1,
+    mysql: {
+        "host": "39.108.68.144",
+        "port": "3306",
+        "database": "blog",
+        "user": "root",
+        "password": "a13660858829",
+        "prefix": "fk_",
+        "type": "mysql"
+    }
 };
